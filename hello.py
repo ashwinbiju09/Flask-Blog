df8fa23de7d536de8eb431079b78f192a7f2c4cf
ashwinbiju09 <20eucs018@skcet.ac.in>
2022-07-14 14:30:37 +0530

    Second Commit

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
+from matplotlib.pyplot import title
+
+posts = [
+    {
+        'author' : 'Creator 1',
+        'title' : 'Post 1',
+        'content' : 'First Post'
+    },
+        {
+        'author' : 'Creator 2',
+        'title' : 'Post 2',
+        'content' : 'Second Post'
+    }
+
+]
 
 
 app = Flask(__name__)
@@ -6,11 +21,11 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
 
 
 if __name__ == '__main__':
